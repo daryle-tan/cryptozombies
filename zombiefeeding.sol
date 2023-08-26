40e3090c0d3ddb279aecb8e42ae69b570b6963be
daryle <daryle.tan@gmail.com>
2023-08-25 21:05:25 -0700

    ch3 3/14

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -23,7 +23,8 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
-    function setKittyContractAddress(address _address) external {
+    // Modify this function:
+    function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     }
 
