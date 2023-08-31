743b905ac2a9bd4050f7acbc3facb15ac2ec3b31
daryle <daryle.tan@gmail.com>
2023-08-30 18:27:02 -0700

    8/13

--- zombieattack.sol
+++ zombieattack.sol
@@ -13,5 +13,14 @@ contract ZombieAttack is ZombieHelper {
             _modulus;
     }
 
-    function attack(uint _zombieId, uint _targetId) external {}
+    // 1. Add modifier here
+    function attack(uint _zombieId, uint _targetId)
+        external
+        ownerOf(_zombieId)
+    {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+
+        uint rand = randMod(100);
+    }
 }
