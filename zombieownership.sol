99a50c69367e48fa3e06ab62d2ef61cc372a306c
daryle <daryle.tan@gmail.com>
2023-08-31 19:23:10 -0700

    ch4 2/15

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,5 +1,9 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {}
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+}
