6303c045cdd894f278c1489f40d24fe75412772e
daryle <daryle.tan@gmail.com>
2023-09-01 18:09:22 -0700

    ch4 8/15

--- zombieownership.sol
+++ zombieownership.sol
@@ -37,12 +37,12 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         _transfer(_from, _to, _tokenId);
     }
 
-    // 1. Add function modifier here
     function approve(address _approved, uint256 _tokenId)
         external
         payable
         onlyOwnerOf(_tokenId)
     {
         zombieApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
     }
 }
