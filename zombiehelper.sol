3a39fa37684bf337f7461b44b550564bb000a9d2
daryle <daryle.tan@gmail.com>
2023-09-01 20:52:47 -0700

    ch4 12/15

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -21,23 +21,21 @@ contract ZombieHelper is ZombieFeeding {
 
     function levelUp(uint _zombieId) external payable {
         require(msg.value == levelUpFee);
-        zombies[_zombieId].level++;
+        zombies[_zombieId].level = zombies[_zombieId].level.add(1);
     }
 
-    // 1. Modify this function to use `ownerOf`:
     function changeName(uint _zombieId, string calldata _newName)
         external
         aboveLevel(2, _zombieId)
-        ownerOf(_zombieId)
+        onlyOwnerOf(_zombieId)
     {
         zombies[_zombieId].name = _newName;
     }
 
-    // 2. Do the same with this function:
     function changeDna(uint _zombieId, uint _newDna)
         external
         aboveLevel(20, _zombieId)
-        ownerOf(_zombieId)
+        onlyOwnerOf(_zombieId)
     {
         zombies[_zombieId].dna = _newDna;
     }
